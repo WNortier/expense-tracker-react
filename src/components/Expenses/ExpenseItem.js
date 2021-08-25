433505457018c873869736c5a2bad365dcd79e81
wnortier <warwick.nortier@gmail.com>
2021-08-25 14:28:59 +0200

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+function ExpenseItem(props) {
+  // const day = 'Car Insurance'
+  // const year = '294.67'
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <div className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Update</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
